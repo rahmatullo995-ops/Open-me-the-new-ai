a58ad7a07f0c5bcdbfb636d6dc2b57a724911ca8
Najmiddin <NajmiddinGulamov>
2026-04-01 10:03:18 +0300

    Hammasi tugadi

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -11,25 +11,25 @@ const Dashboard = () => {
             <div className="Total-Revenue">
                 <a>$10.540</a>
                 <p>Total Revenue</p>
-                <h2>22.45%👇🏻</h2>
+                <h2 className="price-1">22.45%👇🏻</h2>
                 <img src={Dollarimg} alt="Dollar-image" />
             </div>
             <div className="Orders">
                 <a>1,056</a>
                 <p>Orders</p>
-                <h2>15.34%👆🏻</h2>
+                <h2 className="price-1">15.34%👆🏻</h2>
                 <img src={BasketImg} alt="Basket-image" />
             </div>
             <div className="Active-Sessions">
                 <a>48</a>
                 <p>Active Sessions</p>
-                <h2>18.25%👇🏻</h2>
+                <h2 className="price-2">18.25%👇🏻</h2>
                 <img src={HumanImg} alt="Human-image" />
             </div>
             <div>
                 <a>5.420</a>
                 <p>Total Sessions</p>
-                <h2>10.24%👆🏻</h2>
+                <h2 className="price-2">10.24%👆🏻</h2>
                 <img src={PeopleImg} alt="People-image" />
             </div>
         </div>
